19c7417a62c5f5e9881d0e00058a7e4e4a78a28c
Rishabh Tulsian <rishabhtulsian@users.noreply.github.com>
2016-12-16 13:19:25 -0800

    Updated dependent versions

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'patrick.ayoup@gmail.com'
 license          'MIT'
 description      'Installs/Configures virtualbox-guest-additions'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-supports 'centos', '= 6.5'
+supports 'centos', '>= 6.5'
 version          '0.1.0'
 
-depends 'yum-epel', '>= 0.4.0'
+depends 'yum-epel', '>= 1.0.0'
